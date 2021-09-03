1c09172bac61db6e9ad16a8b452f4fd2a0e992ee
fernodarse <fernandonodarsegarcia@gmail.com>
2021-09-03 17:45:59 -0400

    agregando rutas /admin y /login

--- server.js
+++ server.js
@@ -6,5 +6,11 @@ app.use(express.static(__dirname+'/dist/restaurante-paraiso'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/restaurante-paraiso/index.html'));
 });
+app.get('/admin', function(req, res){
+    res.sendFile(__dirname + '/dist/restaurante-paraiso/index.html');
+});
+app.get('/login', function(req, res){
+    res.sendFile(__dirname + '/dist/restaurante-paraiso/index.html');
+});
 
 app.listen(process.env.PORT || 8080);
